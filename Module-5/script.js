285655b2d4a7213204c0414e1ca7c0e62a50117e
smrserg2017 <33425087+smrserg2017@users.noreply.github.com>
2018-03-16 20:28:01 +0200

    Add files via upload

---
+++ Module-5/script.js
@@ -0,0 +1,64 @@
+const alphabetEn = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+const alphabetRu = "йцукенгшщзхъфывапролджэячсмитьбю.";
+const alphabetUa = "йцукенгшщзхїфівапролджєячсмитьбю.";
+
+const getKeyboardArr = (splitChar1, splitChar2, alphabet) => {
+    const arr = alphabet.split('');
+    const keyboard = [arr.slice(0, arr.indexOf(splitChar1)), arr.slice(arr.indexOf(splitChar1), arr.indexOf(splitChar2)), arr.slice(arr.indexOf(splitChar2))];
+    return keyboard;
+}
+const keyboardEn = getKeyboardArr ("a", "z", alphabetEn);
+const keyboardRu = getKeyboardArr ("ф", "я", alphabetRu);
+const keyboardUa = getKeyboardArr ("ф", "я", alphabetUa);
+
+const keyboard = {
+    layouts: {
+        en: {
+            topRow: [],
+            middleRow: [],
+            bottomRow: []
+        },
+        ru: {
+            topRow: [],
+            middleRow: [],
+            bottomRow: []
+        },
+        ua: {
+            topRow: [],
+            middleRow: [],
+            bottomRow: []
+        }
+    },
+    langs: ['en', 'ru', 'ua'],
+    currentLang: ''
+};
+
+const writeKeyboardToObj = (keyboardArr, lang) => keyboard.layouts[lang] = keyboardArr;
+
+writeKeyboardToObj (keyboardEn, "en");
+writeKeyboardToObj (keyboardRu, "ru");
+writeKeyboardToObj (keyboardUa, "ua");
+
+const writeLangToObj = () => {
+let lang = "";
+    do {
+        lang = prompt(' Choose the keyboard layout:  en - 0, ru - 1, ua - 2 ', "");
+    if ( lang === '0' || lang === '1' || lang === '2' ) break;
+    if ( lang === null) return;
+        alert("Input correct value!");
+    } while (true);
+    return keyboard.currentLang = keyboard.langs[lang];
+}
+
+writeLangToObj()
+
+const getRandCharInAlph = () => {
+    const array = keyboard.layouts[keyboard.currentLang];
+    const randomString = Math.floor(Math.random() * array.length);
+    const randomChar = Math.floor(Math.random() * array[randomString].length);
+    return array[randomString][randomChar];
+    
+}
+
+console.log(`Случайный символ ${keyboard.currentLang} алфавита - ${getRandCharInAlph()}`);
+
